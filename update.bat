ab90cefcdf49853cb320cc23395c077104af00f9
Mrkuff <kuff_@hotmail.com>
2019-08-21 09:58:47 -0400

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,14 +1,14 @@
 @echo off
-:: Credits: Credit to Ryan Watson (@gentlemanwatson) and Syspanda.com from which this script was adapted from.
-:: Credit also to @S0xbad1dea for their input which has been merged here from https://github.com/ukncsc/lme/pull/4
-:: Modified for 32 bit vs 64 bit, different install path and few security change Jonathan Martineau
+REM Credits: Credit to Ryan Watson (@gentlemanwatson) and Syspanda.com from which this script was adapted from.
+REM Credit also to @S0xbad1dea for their input which has been merged here from https://github.com/ukncsc/lme/pull/4
+REM Modified for 32 bit vs 64 bit, different install path and few security change Jonathan Martineau
 
-:: Ive move the file inside the GPO instead of a dedicated structure in SYSVOL. GUID of GPO is set here
+REM Ive move the file inside the GPO instead of a dedicated structure in SYSVOL. GUID of GPO is set here
 SET GUID={99999999-9999-9999-9999-999999999999}
 
-:: Finding OS Architecture
-:: https://support.microsoft.com/en-ca/help/556009
-:: Not using %OS% because its already used
+REM Finding OS Architecture
+REM https://support.microsoft.com/en-ca/help/556009
+REM Not using %OS% because its already used
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set NOS=32BIT || set NOS=64BIT
 IF %NOS%==32BIT (
 SET SYSMONBIN=Sysmon.exe
@@ -17,32 +17,32 @@ SET SIGCHECK=sigcheck.exe
 SET SYSMONBIN=Sysmon64.exe
 SET SIGCHECK=sigcheck64.exe
 )
-ECHO %SYSMONBIN%
-ECHO %SIGCHECK%
-:: %~nI - Expands %I to a file name only. added for debug in command line FOR %i IN ("%SYSMONBIN%") DO (set PROCESS=%~nI)
-FOR %%i IN ("%SYSMONBIN%") DO (set PROCESS=%%~nI)
-ECHO %PROCESS%
+REM ECHO %SYSMONBIN%
+REM ECHO %SIGCHECK%
 
-:: Finding Domain FQDN
+FOR %%i IN ("%SYSMONBIN%") DO (set PROCESS=%%~ni)
+REM ECHO %PROCESS%
+
+REM Finding Domain FQDN
 (wmic computersystem get domain | findstr /v Domain | findstr /r /v "^$") > %Windir%\Logs\fqdn.txt
 SET /p FQDN=<%Windir%\Logs\fqdn.txt
 SET FQDN=%FQDN: =%
-ECHO %FQDN%
+REM ECHO %FQDN%
 
 SET SYSMONCONF=sysmon.xml
 
-:: Setting SYSVOL Path from GPO
+REM Setting SYSVOL Path from GPO
 SET GLBSYSMONBIN=\\%FQDN%\sysvol\%FQDN%\Policies\%GUID%\Machine\Scripts\%SYSMONBIN%
 SET GLBSYSMONCONFIG=\\%FQDN%\sysvol\%FQDN%\Policies\%GUID%\Machine\Scripts\%SYSMONCONF%
 SET GLBSIGCHECK=\\%FQDN%\sysvol\%FQDN%\Policies\%GUID%\Machine\Scripts\%SIGCHECK%
 
-:: Is Sysmon running  
+REM Is Sysmon running  
 sc query %PROCESS% | find "STATE" | find "RUNNING"
 IF "%ERRORLEVEL%" NEQ "0" (
-:: No, lets try to start it
+REM No, lets try to start it
 GOTO startsysmon
 ) ELSE (
-:: Yes, Lets see if it needs updating
+REM Yes, Lets see if it needs updating
 GOTO checkversion
 )
 
@@ -50,56 +50,55 @@ GOTO checkversion
 :startsysmon
 sc start %PROCESS%
 IF "%ERRORLEVEL%" EQU "1060" (
-:: Wont start, Lets install it
+REM Wont start, Lets install it
 GOTO installsysmon
 ) ELSE (
-:: Started, Lets see if it needs updating
+REM Started, Lets see if it needs updating
 GOTO checkversion
 )
 
 
 :installsysmon
-:: Changing the directory for C:\windows\Sysmon since we install/reinstall
+REM Changing the directory for C:\windows\Sysmon since we install/reinstall
 SET SYSMONDIR=C:\windows\Sysmon
 IF Not EXIST %SYSMONDIR% (
 mkdir %SYSMONDIR%
 )
-xcopy %GLBSYSMONBIN% %SYSMONDIR% /y
-xcopy %GLBSYSMONCONFIG% %SYSMONDIR% /y
-xcopy %GLBSIGCHECK% %SYSMONDIR% /y
+xcopy %GLBSYSMONBIN% %SYSMONDIR% /y /q
+xcopy %GLBSYSMONCONFIG% %SYSMONDIR% /y /q
+xcopy %GLBSIGCHECK% %SYSMONDIR% /y /q
 chdir %SYSMONDIR%
-:: We put the Logs in %Windir%\Logs\
-echo "==========================================" >> %Windir%\Logs\SysmonInstall.log
-echo %date% %time% >> %Windir%\Logs\SysmonInstall.log
+REM We put the Logs in %Windir%\Logs\
+ECHO "==========================================" >> %Windir%\Logs\SysmonInstall.log
+ECHO %date% %time% >> %Windir%\Logs\SysmonInstall.log
 %SYSMONBIN% -i %SYSMONCONF% -accepteula -h md5,sha256 -n -l >> %Windir%\Logs\SysmonInstall.log
-echo "==========================================" >> %Windir%\Logs\SysmonInstall.log
-:: We modified also the GPO for settings the service startup
+ECHO "==========================================" >> %Windir%\Logs\SysmonInstall.log
 sc config %PROCESS% start= auto
-:: We exit here since we dont need to check or update it since its brand new
+REM We exit here since we dont need to check or update it since its brand new
 EXIT /B 0
 
 
 :checkversion
-:: Check if %SYSMONBIN% matches the hash of the central version 
+REM Check if %SYSMONBIN% matches the hash of the central version 
 
-:: Finding Installed Directory
+REM Finding Installed Directory
 (wmic process where "name='%SYSMONBIN%'" get ExecutablePath | findstr /v ExecutablePath) > %Windir%\Logs\path.txt
 SET /p EXECUTABLEPATH=<%Windir%\Logs\path.txt
-echo %EXECUTABLEPATH%
-:: %~dpI - Expands %I to a drive letter and path only. added for debug in command line FOR %i IN ("%EXECUTABLEPATH%") DO (set SYSMONDIR=%~dpI)
-FOR %%i IN ("%EXECUTABLEPATH%") DO (set SYSMONDIR=%%~dpI)
-echo %SYSMONDIR%
+REM ECHO %EXECUTABLEPATH%
+
+FOR %%i IN ("%EXECUTABLEPATH%") DO (set SYSMONDIR=%%~dpi)
+REM ECHO %SYSMONDIR%
 
 chdir %SYSMONDIR%
-:: We want to be sure it's the right %SIGCHECK% from SYSVOL
-xcopy %GLBSIGCHECK% %SYSMONDIR% /y
+REM We want to be sure it's the right %SIGCHECK% from SYSVOL.  But this mean we download it everytime the script is run
+xcopy %GLBSIGCHECK% %SYSMONDIR% /y /q
 
 (%SIGCHECK% -n -nobanner /accepteula %SYSMONBIN%) > %SYSMONDIR%\runningver.txt
 (%SIGCHECK% -n -nobanner /accepteula %GLBSYSMONBIN%) > %SYSMONDIR%\latestver.txt
-set /p runningver=<%SYSMONDIR%\runningver.txt
-set /p latestver=<%SYSMONDIR%\latestver.txt
-echo Currently running Sysmon : %runningver%
-echo Latest sysmon is %latestver% located at %GLBSYSMONBIN%
+SET /p runningver=<%SYSMONDIR%\runningver.txt
+SET /p latestver=<%SYSMONDIR%\latestver.txt
+REM ECHO Currently running Sysmon : %runningver%
+REM ECHO Latest sysmon is %latestver% located at %GLBSYSMONBIN%
 IF "%runningver%" NEQ "%latestver%" (
 GOTO uninstallsysmon
 ) ELSE (
@@ -108,17 +107,17 @@ GOTO updateconfig
 
 
 :updateconfig
-:: Added -c for the comparison, enables us to compare hashes
+REM Added -c for the comparison, enables us to compare hashes
 (%SIGCHECK% -h -c -nobanner /accepteula %SYSMONCONF%) > %SYSMONDIR%\runningconfver.txt
 (%SIGCHECK% -h -c -nobanner /accepteula %GLBSYSMONCONFIG%) > %SYSMONDIR%\latestconfver.txt
-::Looks for the 11th token in the csv of sigcheck. This is the MD5 hash. 12th token is SHA1, 15th is SHA2
-:: added for debug in command line FOR /F "delims=, tokens=15" %h in (runningconfver.txt) DO (set runningconfver=%h)
+REM Looks for the 11th token in the csv of sigcheck. This is the MD5 hash. 12th token is SHA1, 15th is SHA2
+
 FOR /F "delims=, tokens=15" %%h in (runningconfver.txt) DO (set runningconfver=%%h)
-:: added for debug in command line FOR /F "delims=, tokens=15" %h in (latestconfver.txt) DO (set latestconfver=%h)
+
 FOR /F "delims=, tokens=15" %%h in (latestconfver.txt) DO (set latestconfver=%%h)
 
 IF "%runningconfver%" NEQ "%latestconfver%" (
-xcopy %GLBSYSMONCONFIG% %SYSMONCONF% /y
+xcopy %GLBSYSMONCONFIG% %SYSMONCONF% /y /q
 %SYSMONBIN% -c %SYSMONCONF%
 )
 EXIT /B 0
